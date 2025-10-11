1186277ce6f8ec1350a2d22963d0cedbcad46a6f
dvillano2 <domvillano@gmail.com>
2025-10-11 12:19:31 -0400

    sorting structures

---
+++ structures.py
@@ -0,0 +1,63 @@
+from typing import List
+from typing import Tuple
+from itertools import product
+
+
+def get_total_points(prime: int) -> int:
+    return prime**3
+
+
+def get_total_directions(prime: int) -> int:
+    return sum([prime**2, prime, 1])
+
+
+def expand_index(index: int, prime: int) -> Tuple[int, int, int]:
+    z, xy = divmod(index, prime**2)
+    y, x = divmod(xy, prime)
+    return (x, y, z)
+
+
+def shift_point(point: int, shift: int, prime: int) -> int:
+    "returns point + int, all in index form"
+    point_coords = expand_index(point, prime)
+    shift_coords = expand_index(shift, prime)
+    shifted_point = 0
+    for i, (point_coord, shift_coord) in enumerate(
+        zip(point_coords, shift_coords)
+    ):
+        shifted_point += ((point_coord + shift_coord) % prime) * (prime**i)
+    return shifted_point
+
+
+def non_flat_line_translates(
+    prime: int, line: List[int]
+) -> List[List[List[int]]]:
+    "Line is assumed to be a line pointing outside of the x y plane"
+    total_points = get_total_points(prime)
+    translates = [[[0] for _ in range(prime)] for _ in range(prime)]
+    for x in range(prime):
+        for y in range(prime):
+            translates[y][x] = [
+                (point + x + prime * y) % total_points for point in line
+            ]
+    return translates
+
+
+def non_flat_lines_through_origin(prime: int) -> List[List[List[int]]]:
+    stereo_directions = [[[0] for _ in range(prime)] for _ in range(prime)]
+    for x in range(prime):
+        for y in range(prime):
+            stereo_directions[y][x] = [
+                ((t * x) % prime + ((t * y) % prime) * prime + t * prime**2)
+                for t in range(prime)
+            ]
+    return stereo_directions
+
+
+def all_non_flat_lines(prime: int) -> List[List[List[List[List[int]]]]]:
+    origin_lines = non_flat_lines_through_origin(prime)
+    spot_holder = [[[0] for _ in range(prime)] for _ in range(prime)]
+    full_list = [[spot_holder for _ in range(prime)] for _ in range(prime)]
+    for x, y, z, w in product(range(prime), repeat=4):
+        full_list[y][x] = non_flat_line_translates(prime, origin_lines[w][z])
+    return full_list
