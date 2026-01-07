35a6169a64463363b50ad1e78e6b9cd84edfebe6
dvillano2 <domvillano@gmail.com>
2026-01-07 17:29:48 -0500

    retract

--- animation.py
+++ animation.py
@@ -3,12 +3,8 @@ from PIL import Image
 
 if __name__ == "__main__":
     frames = [Image.open(f) for f in sorted(glob.glob("images/*.png"))]
-    frames_resized = [
-        f.resize((f.width // 2, f.height // 2), Image.Resampling.LANCZOS)
-        for f in frames
-    ]
 
-    frames_resized[0].save(
+    frames[0].save(
         "animation/animation.gif",
         save_all=True,
         append_images=frames[1:],
